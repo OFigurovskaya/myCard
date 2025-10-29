6b7d6cb80abd21f234037c3c95649a31b6af1ed2
Olesya Figurovskaya <lisonkadrug@gmail.com>
2025-10-29 17:08:04 +0300

    changed sruction

--- src/pages/MyProjects.vue
+++ src/pages/MyProjects.vue
@@ -7,25 +7,42 @@ import getText from '../stores/data.js';
 const indexPage = ref(3);
 const text = getText().projects;
 const textEnd = getText().projectsText;
+const activeIndex = ref(null);
+
+const toggleProject = (index) => {
+    if (activeIndex.value === index) {
+        activeIndex.value = null;
+    } else {
+        activeIndex.value = index;
+    }
+};
 
 </script>
 
 <template>
-   <div class="wrapper">
+    <div class="wrapper">
         <BaseHeader :indexPage="indexPage" />
         <div class="main projects">
             <h1 class="projects__header">Мои проекты:</h1>
-            <div class="project" v-for="elem in text" :key="elem.id">
-                <p class="project__name">{{ elem.name }}</p>
-                <p class="project__descr">{{ elem.descr }}</p>
-                <p class="project__stack">{{ elem.stack }}</p>
-                <img class="project__img" :src="elem.img" alt="elem.img">
-                <p class="project__link"><a target="_blank" :href="elem.link">{{ elem.link }}</a>
+            <div class="project" v-for="(elem, index) in text" :key="index">
+                <h2 class="project__name" @click="toggleProject(index)">{{ elem.name }} </h2>
+                <div class="accordion" v-if="activeIndex === index">
+                    <p class="project__descr"> {{ elem.descr }}
+                    <ul v-for="(elem, index) in elem.func" :key="index">Функционал:
+                        <li class="marker">{{ elem }}</li>
+                    </ul>
                     </p>
-                <p class="project__img"><a target="_blank" :href="elem.git">{{ elem.git }}</a>
-                </p>
+                    <p class="project__stack">Cтек: 
+                        <ul v-for="(elem, index) in elem.stack" :key="index">
+                        <li class="marker">{{ elem }}</li>
+                    </ul></p>
+                    <p class="project__link">Ссылка на деплой: <a target="_blank" :href="elem.link">{{ elem.link }}</a>
+                    </p>
+                    <p class="project__git">Ссылка на проект на гите:<a target="_blank" :href="elem.git">{{ elem.git }}</a>
+                    </p>
+                    <img class="project__img" :src="elem.img" alt="elem.img">
+                </div>
             </div>
         </div>
         <BaseFooter />
-    </div>
-</template>
+</div></template>
