b124df3b6179e4e319d5aa9cf35e4f74d12e1bdf
Olesya Figurovskaya <lisonkadrug@gmail.com>
2025-10-29 12:16:30 +0300

    PersonalPage is fished

--- src/pages/PersonalData.vue
+++ src/pages/PersonalData.vue
@@ -2,14 +2,19 @@
 import { ref } from 'vue';
 import BaseHeader from '../components/BaseHeader.vue';
 import BaseFooter from '../components/BaseFooter.vue';
+import getText from '../stores/data.js';
+
 const indexPage = ref(2);
+const text = getText().personalData;
+
 </script>
 
 <template>
     <div class="wrapper">
         <BaseHeader :indexPage="indexPage" />
         <div class="main personal">
-            Personal
+            <h1 class="personal__header">Подробне обо мне:</h1>
+            <p class="personal__text" v-for="elem in text" :key="elem.id">{{ elem }}</p>
         </div>
         <BaseFooter />
     </div>
